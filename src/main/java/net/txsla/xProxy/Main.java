0988045a270fe9ed66d0442769aa09e06a657ef4
_Txsla <104021913+Tesla6426@users.noreply.github.com>
2024-12-07 16:01:02 -0800

    a couple small changes

--- src/main/java/net/txsla/xProxy/Main.java
+++ src/main/java/net/txsla/xProxy/Main.java
@@ -6,16 +6,18 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
         // add config for these two
         int port = 25599;
         Server.password = "P@ssword";
-
+        ServerSocket serverSocket;
 
         System.out.println("Starting xProxy...");
-        ServerSocket serverSocket = new ServerSocket(port);
-        Server server = new Server(serverSocket);
-        server.startServer();
+        try {
+            serverSocket = new ServerSocket(port);
+            Server server = new Server(serverSocket);
+            server.startServer();
+        } catch  (Exception e) { e.printStackTrace();}
     }
 }
